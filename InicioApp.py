bfee98b5a78083a889986f54b8daab326ed406ab
Marcosher249 <marcosher249@alu.centroafuera.es>
2023-06-12 03:18:38 +0200

    actualizacion2

--- InicioApp.py
+++ InicioApp.py
@@ -5,6 +5,8 @@ from ui_bottrade import *
 from PySide2.QtGui import QPainter
 from PySide2.QtCharts import QtCharts
 
+from Custom_Widgets.Widgets import *
+
 from functools import partial
 
 # if __name__ == '__main__':
@@ -16,20 +18,28 @@ elementos_ocultos = {
     "menu",
     "frame_3",
     "frame_4",
-    "frame_6"
+    "frame_6",
+    "frame_7"
 }
 
-class MainWindow(QMainWindow()):
+class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.show
+        self.setMinimumSize(850, 600)
+        self.show()
+        for x in elementos_ocultos:
+            effect = QtWidgets.QGraphicsDropShadowEffect(self)
+            effect.setBlurRadius(18)
+            effect.setXOffset(0)
+            effect.setYOffset(0)
+            effect.setColor(QColor(0,0,0,255))
+            getattr(self.ui, x).setGraphicsEffect(effect)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    window = MainWindow()
+    window = MainWindow()  # Utiliza la instancia de MainWindow existente
     window.show()
     sys.exit(app.exec_())
 
